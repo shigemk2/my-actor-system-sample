47d9b6f453a234681aa26074c867324b02ab251b
shigemk2 <i.am.shige@gmail.com>
2016-08-15 17:23:00 +0900

    Fixed Error: Move application.conf from conf to src/main/resources

--- src/main/scala/com/example/ReferenceApp.scala
+++ src/main/scala/com/example/ReferenceApp.scala
@@ -7,11 +7,11 @@ object ReferenceApp extends App {
   override def main(args: Array[String]): Unit = {
     implicit val system = ActorSystem.apply("ReferenceApp")
     // router1配下に$a,$b,$cのActorが生成される
-    // val router1 = system.actorOf(FromConfig.props(Props[MessagePrintActor]), "router1")
-    val router1 = system.actorOf(Props[MessagePrintActor], "router1")
+    val router1 = system.actorOf(FromConfig.props(Props[MessagePrintActor]), "router1")
+    // val router1 = system.actorOf(Props[MessagePrintActor], "router1")
 
-    // val actor1 = system.actorSelection("akka://ReferenceApp/user/router1/$b")
-    // actor1 ! "Message1"
+    val actor1 = system.actorSelection("akka://ReferenceApp/user/router1/$b")
+    actor1 ! "Message1"
 
     router1 ! "Message2"
     router1 ! "Message3"
