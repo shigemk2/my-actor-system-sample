5c2b1740c09a18ded0ea55de86e5c982b8fd0ea4
shigemk2 <i.am.shige@gmail.com>
2016-08-15 15:54:21 +0900

    Add ReferenceApp

---
+++ src/main/scala/com/example/ReferenceApp.scala
@@ -0,0 +1,22 @@
+package com.example
+
+import akka.actor.{ActorSystem, Props}
+import akka.routing.FromConfig
+
+object ReferenceApp extends App {
+  override def main(args: Array[String]): Unit = {
+    implicit val system = ActorSystem.apply("ReferenceApp")
+    // router1配下に$a,$b,$cのActorが生成される
+    val router1 = system.actorOf(FromConfig.props(Props[MessagePrintActor]),
+    "router1")
+
+    val actor1 = system.actorSelection("akka://ReferenceApp/user/router1/$b")
+    actor1 ! "Message1"
+
+    router1 ! "Message2"
+    router1 ! "Message3"
+
+    Thread.sleep(1000)
+    system.shutdown()
+  }
+}
