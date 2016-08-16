ee9c878e9d10529c0cc85fa39b20c64e8315dde7
shigemk2 <i.am.shige@gmail.com>
2016-08-17 00:25:52 +0900

    deadline passed

--- src/main/scala/com/example/ReferenceApp.scala
+++ src/main/scala/com/example/ReferenceApp.scala
@@ -8,14 +8,10 @@ import scala.concurrent.TimeoutException
 object ReferenceApp extends App {
   override def main(args: Array[String]): Unit = {
     implicit val system = ActorSystem.apply("ReferenceApp")
-    // router1配下に$a,$b,$cのActorが生成される
-    val router1 = system.actorOf(FromConfig.props(Props[MessagePrintActor]), "router1")
-    // val router1 = system.actorOf(Props[MessagePrintActor], "router1")
+    // router1配下に$a、$b、$cのActorが生成される
+    val router1 = system.actorOf(FromConfig.props(Props[MessagePrintActor]),
+      "router1")
 
-    // フルパス
-    // val actor1 = system.actorSelection("akka://ReferenceApp/user/router1/$b")
-    // 相対パス
-    // val actor1 = system.actorSelection("/user/router1/$b")
     val actors = system.actorSelection("/user/router1/*")
     val rootInbox = ActorDSL.inbox()
     actors.tell("Path1", rootInbox.getRef())
