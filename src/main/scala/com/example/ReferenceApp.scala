38f69aafe3b08fe87855c050ee68d92c6b6a145e
shigemk2 <i.am.shige@gmail.com>
2016-08-17 00:24:50 +0900

    Fix

--- src/main/scala/com/example/ReferenceApp.scala
+++ src/main/scala/com/example/ReferenceApp.scala
@@ -3,6 +3,8 @@ package com.example
 import akka.actor.{ActorDSL, ActorSystem, Props}
 import akka.routing.FromConfig
 
+import scala.concurrent.TimeoutException
+
 object ReferenceApp extends App {
   override def main(args: Array[String]): Unit = {
     implicit val system = ActorSystem.apply("ReferenceApp")
@@ -19,11 +21,28 @@ object ReferenceApp extends App {
     actors.tell("Path1", rootInbox.getRef())
     actors.tell("Path2", rootInbox.getRef())
 
-    // actor1 ! "Message1"
-    actors ! "Message1"
+    // 非同期処理のため、以後のreceiveがPath2の到着前に実行されるのを防止するために待ちを入れる
+    Thread.sleep(1000)
 
-    router1 ! "Message2"
-    router1 ! "Message3"
+    val received1 = rootInbox.receive()
+    println("received1:" + received1)
+    val received2 = rootInbox.receive()
+    println("received2:" + received2)
+    val received3 = rootInbox.receive()
+    println("received3:" + received3)
+    try {
+      val received4 = rootInbox.receive()
+      println("received4:" + received4)
+    }
+    catch {
+      case ex:TimeoutException => { println("Exception Occured." + ex.getMessage)}
+    }
+
+    actors.tell("Path3", rootInbox.getRef())
+    val received5 = rootInbox.receive()
+    println("received5:" + received5)
+
+    router1 ! "Test1"
 
     Thread.sleep(1000)
     system.shutdown()
